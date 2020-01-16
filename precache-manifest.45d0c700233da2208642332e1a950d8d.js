39fa18857006193a5f6ea103c2e09bf54439d006
huseyintasci <huseyintasci@yandex.com>
2020-01-16 05:03:07 -0800

    Updates

---
+++ precache-manifest.45d0c700233da2208642332e1a950d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7e33be636d76dc7d6da027b559b16f",
+    "url": "/westelm/index.html"
+  },
+  {
+    "revision": "c6fdc689f3a66f7216bc",
+    "url": "/westelm/static/css/main.b67d4b3d.chunk.css"
+  },
+  {
+    "revision": "bdcf2214030826a1d992",
+    "url": "/westelm/static/js/2.5b998222.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/westelm/static/js/2.5b998222.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6fdc689f3a66f7216bc",
+    "url": "/westelm/static/js/main.f4118a91.chunk.js"
+  },
+  {
+    "revision": "d7950eed76a4fd7aa763",
+    "url": "/westelm/static/js/runtime-main.4fc2e10e.js"
+  }
+]);
